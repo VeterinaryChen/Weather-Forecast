8c693b33be94e93dc61896efd6eaf0838b06a644
VeterinaryChen <513100163@qq.com>
2019-08-04 23:59:35 +0800

    update

--- src/main/java/com/ls/spring/cloud/service/impl/WeatherDataServiceImpl.java
+++ src/main/java/com/ls/spring/cloud/service/impl/WeatherDataServiceImpl.java
@@ -5,23 +5,32 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ls.spring.cloud.service.WeatherDataService;
 import com.ls.spring.cloud.vo.WeatherResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: jeffchen
  */
 @Service
 public class WeatherDataServiceImpl implements WeatherDataService {
-
-    private final String WEATHER_URI = "http://wthrcdn.etouch.cn/weather_mini?";
+    private final static Logger Logger = LoggerFactory.getLogger(WeatherDataServiceImpl.class);
+    private static final String WEATHER_URI = "http://wthrcdn.etouch.cn/weather_mini?";
+    private static final long TIME_OUT = 10L;
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     /**
      * 功能描述: 根据城市 ID 查询城市天气
@@ -53,26 +62,40 @@ public class WeatherDataServiceImpl implements WeatherDataService {
     }
 
     private WeatherResponse doGetWeather (String uri) {
-        WeatherResponse weatherResponse = null;
-        ResponseEntity<String> respString = restTemplate.getForEntity(uri, String.class);
-        ObjectMapper mapper = new ObjectMapper();
+        String key = uri;
         String strbody = null;
-
-        if (respString.getStatusCodeValue() == 200) {
-            strbody = respString.getBody();
-
+        WeatherResponse weatherResponse = null;
+        ObjectMapper mapper = new ObjectMapper();
+        // 先查缓存，再调接口
+        ValueOperations <String,String> ops = stringRedisTemplate.opsForValue();
+        if (stringRedisTemplate.hasKey(key)){
+            Logger.info("Redis has Data!");
+            strbody = ops.get(key);
         }
+        else {
 
-        try {
-            weatherResponse = mapper.readValue(strbody, WeatherResponse.class);
-        } catch (JsonParseException e) {
+            ResponseEntity<String> respString = restTemplate.getForEntity(uri, String.class);
+
+            if (respString.getStatusCodeValue() == 200) {
+                strbody = respString.getBody();
+
+            }
+
+            // 将数据写入缓存
+            ops.set(key, strbody, TIME_OUT, TimeUnit.SECONDS);
+
+            try {
+                weatherResponse = mapper.readValue(strbody, WeatherResponse.class);
+            } catch (JsonParseException e) {
             e.printStackTrace();
         } catch (JsonMappingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return weatherResponse;
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                Logger.error("Redis has no Data!");
+            }
+            }
+            return weatherResponse;
     }
 }
